4406532e6edf309538eacd858c723f2fbc96bcc0
Jacob Weate <jacob.weate@accenture.com>
2020-06-01 15:41:43 -0400

    fixed min platform version issue

--- docker/corda/init_node.sh
+++ docker/corda/init_node.sh
@@ -1,10 +1,3 @@
 
-echo "Starting corda node for PartyA..."
+echo "Starting corda nodes..."
 java -jar /opt/corda/partyA/corda.jar --base-directory=/opt/corda/partyA & java -jar /opt/corda/partyB/corda.jar --base-directory=/opt/corda/partyB && fg
-#
-#P1=$!
-#
-#sleep 60
-#echo "Adding node info to volume"
-#cp /opt/corda/nodeInfo-* /opt/corda/additional-node-infos
-#fg $P1
